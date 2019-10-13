fcbeb80737c021c66d51df9760adc5558b261043
Michael Rose <32867819+RoseBush1404@users.noreply.github.com>
2019-10-13 16:28:58 +0100

    added melee weapon class

---
+++ Duskfall/Source/Duskfall/Weapons/MeleeWeapon.cpp
@@ -0,0 +1,5 @@
+// Duskfall - Michael Rose - r015676g
+
+
+#include "MeleeWeapon.h"
+
---
+++ Duskfall/Source/Duskfall/Weapons/MeleeWeapon.h
@@ -0,0 +1,17 @@
+// Duskfall - Michael Rose - r015676g
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/BaseWeapon.h"
+#include "MeleeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUSKFALL_API AMeleeWeapon : public ABaseWeapon
+{
+	GENERATED_BODY()
+	
+};
